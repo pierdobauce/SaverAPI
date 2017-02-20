31892b0c131fa663bd7913c7b81ad867281f23b9
opbo6311 <opbo6311@EB-OR7008523.nice.francetelecom.fr>
2017-02-21 00:03:55 +0100

    feb 20 soir

--- MyTestProject/src/root/nested/nourriture.py
+++ MyTestProject/src/root/nested/nourriture.py
@@ -6,7 +6,7 @@ Created on 30 nov. 2016
 from root.nested.habitant import Habitant
 
 class Nourriture(Habitant):
-    "De la nourriture pour le cobaye dans le monde virtuel"
+    "De la nourriture pour le cobaye dans le monde virtuel" 
 
 
     def __init__(self, ter, posx, posy):
